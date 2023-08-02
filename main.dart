d7a094018fac369c663a2e8a9ff4a0f50c706e38
mhsaurov <skmehedihassan58@gmail.com>
2023-08-02 20:32:50 +0600

    assignment has been done

--- main.dart
+++ main.dart
@@ -1,3 +1,24 @@
 void main(){
-  
+  int a = 17;
+  int b = 13;
+
+  int add = a + b;
+  int sub = a - b;
+  int multi = a * b;
+  int divi = a ~/ b;
+  int mod = a % b;
+
+
+  // here is all output
+
+  print('The Addition: $add');
+
+  print('The Subtraction is: $sub');
+
+  print('The Multiplication: $multi');
+
+  print('The Division is: $divi');
+
+  print('The Modulus is: $mod');
+
 }
